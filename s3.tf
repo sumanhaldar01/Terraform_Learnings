aa302b2f773219b5c6f95133cfb682dcbcb41388
Suman Haldar <103129532+sumanhaldar01@users.noreply.github.com>
2025-11-20 17:21:59 +0530

    Add S3 bucket versioning and ACL configuration
    Added versioning and ACL resources for S3 bucket.

--- s3.tf
+++ s3.tf
@@ -1,4 +1,14 @@
-resource "aws_s3_bucket" "my_bucket"{
+resource "aws_s3_bucket" "my_bucket" {
     bucket ="Terraform-Oneshot-Bucket-007"
     acl = "private"
+}
+resource "aws_s3_bucket_versioning" "versioning" {
+    bucket = aws_s3_bucket.my_bucket.id
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
 }
