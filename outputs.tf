fc8022dfaf311b98c1780c88c7de0b6f60dd28a3
Suman Haldar <103129532+sumanhaldar01@users.noreply.github.com>
2025-11-20 15:06:29 +0530

    updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "ec2_instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.my_instance.id
+}
 output "ec2_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.my_instance.public_ip
