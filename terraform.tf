c6bb6d2737ce4cb3780bb22eb421eaed30f9a6e7
Suman Haldar <103129532+sumanhaldar01@users.noreply.github.com>
2026-01-02 12:26:26 +0530

    Add TLS and local providers to terraform.tf
    Added TLS and local providers to Terraform configuration.

--- terraform.tf
+++ terraform.tf
@@ -4,8 +4,18 @@ terraform{
             source = "hashicorp/aws"
             version = "5.91.0"
         }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
     }
 }
 
 # Execution plan was stored in plan.out
+
 # terrform fmt reformats configuration to canonical format and style
