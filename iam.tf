9def139a45b996a4f8e5f3f34e373a96b833b639
Suman Haldar <103129532+sumanhaldar01@users.noreply.github.com>
2026-01-12 14:51:18 +0530

    Add IAM user, group, and policy for Terraform

--- iam.tf
+++ iam.tf
@@ -1,24 +1,34 @@
-resource "aws_iam_user" "test"{
-  name = "iam_suman"
+resource "aws_iam_user" "terraform_user"{
+    name = "terraform-user"
 }
-resource "aws_iam_group" "test"{
-  name = "iam_devops"
+
+resource "aws_iam_group" "terraform_group"{
+    name = "terraform-group"
 }
-resource "aws_iam_policy" "policy" {
-  name        = "ec2_readonly_policy"
-  #path        = "/"
-  #description = "My test policy"
+
+resource "aws_iam_policy" "terraform_policy" {
+  name = "terraform-policy"
+  description = "Policy for Terraform User"
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
+        Effect = "Allow"
+        #Action =["ec2:*"", "s3:*", vpc:*, "cloudwatch:*"]
+        # Action = [
+        #   "s3:ListBucket",
+        #   "s3:GetObject",
+        #   "s3:PutObject",
+        #   "s3:DeleteObject"
+        # ]
+        # Resource = [
+        #   aws_s3_bucket.my_bucket.arn,
+        #   "${aws_s3_bucket.my_bucket.arn}/*"
+        # ]
+        Action = ["ec2.Describe*"]
+        Resource = ["*"]
+      }
     ]
   })
 }
